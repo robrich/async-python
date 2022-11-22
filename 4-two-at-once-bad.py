3a5f93ee73f5f48a50aed8b96bdbab0775df5caa
Rob Richardson <robrich@robrich.org>
2022-11-22 13:52:48 -0700

    Add white space

--- 4-two-at-once-bad.py
+++ 4-two-at-once-bad.py
@@ -11,9 +11,11 @@ async def main():
     print('calling wait for 2 seconds')
     res2 = await do_work(2)
     print('exited wait for 2 seconds')
+
     print('calling wait for 1 second')
     res1 = await do_work(1)
     print('exited wait for 1 second')
+
     print('finished')
     print(f"res2: {res2}, res1: {res1}")
 
